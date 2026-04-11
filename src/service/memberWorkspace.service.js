c4345a862528af061bd38913cdae877dbf37202e
estban03 <estebanmutuverria003@gmail.com>
2026-04-11 17:21:16 -0300

    memeberWorkspaceService.js fixed

--- src/service/memberWorkspace.service.js
+++ src/service/memberWorkspace.service.js
@@ -6,13 +6,12 @@ class MemberWorkspaceService {
     async getWorkspaces(user_id) {
         //traer la lista de espacios de trabajo relacionados a el usuario logueado
         const workspacesList = await memberWorkspaceRepository.getWorkspaceListByUserId(user_id)
-        console.log("Workspaces List: ", workspacesList)
         return workspacesList
     }
 
     async create(workspace_id, user_id, role) {
         const result = await memberWorkspaceRepository.getUserByWorkspaceIdAndUserId(workspace_id, user_id)
-        if (result.length > 0) {
+        if (result) {
             throw new ServerError('El usuario ya existe en este espacio de trabajo', 400)
         }
 
