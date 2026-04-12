b101cbb0c9ad58156dd841c6c02d84ee0c56c909
estban03 <estebanmutuverria003@gmail.com>
2026-04-12 01:36:52 -0300

    feat: initialize memberWorkspaces route with workspace management endpoints

--- src/routes/memberWorkspaces.route.js
+++ src/routes/memberWorkspaces.route.js
@@ -2,15 +2,15 @@ import express from 'express'
 import workspaceController from '../controllers/workspace.controller.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
 import verifyMemberWorkspace from '../middlewares/verifyMemberWorkspaceMiddelware.js'
+import AVILABLE_ROLES from '../constants/roles.constants.js'
 const memberWorkspacesRouter = express.Router()
 
-memberWorkspacesRouter.get
-    (
-        '/',
-        authMiddleware,
-        workspaceController.getWorkspaces
+memberWorkspacesRouter.get(
+    '/',
+    authMiddleware,
+    workspaceController.getWorkspaces
 
-    )
+)
 
 memberWorkspacesRouter.post(
     '/',
@@ -24,5 +24,26 @@ memberWorkspacesRouter.get(
     verifyMemberWorkspace([]),
     workspaceController.getWorkspacebyId
 )
+
+/* memberWorkspacesRouter.put(
+    '/:workspace_id/',
+    authMiddleware,
+    verifyMemberWorkspace([AVILABLE_ROLES.OWNER, AVILABLE_ROLES.ADMIN]),
+    workspaceController.updateMemberById
+)
+
+memberWorkspacesRouter.put(
+    '/:workspace_id/deleteLogic',
+    authMiddleware,
+    verifyMemberWorkspace([AVILABLE_ROLES.OWNER, AVILABLE_ROLES.ADMIN]),
+    workspaceController.deleteMemberLogic
+)
+
+memberWorkspacesRouter.delete(
+    '/:workspace_id/deleteFisic',
+    authMiddleware,
+    verifyMemberWorkspace([AVILABLE_ROLES.OWNER, AVILABLE_ROLES.ADMIN]),
+    workspaceController.deleteFisic
+) */
 
 export default memberWorkspacesRouter
